2694234c2cf19c63e0162a03fabf78082a74dd4e
iseoluwadoyin <iseoluwadoyinemmanuel@gmail.com>
2022-10-01 12:45:09 +0000

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,10 +4,4 @@ for number in range(100):
         print("{}{}".format(int(number / 10), number % 10), end="")
         if (number != 89):
             print(", ", end="")
-print("")#!/usr/bin/python3
-for number in range(100):
-    if int(number / 10) != number % 10 and int(number / 10) < number % 10:
-        print("{}{}".format(int(number / 10), number % 10), end="")
-        if (number != 89):
-            print(", ", end="")
 print("")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,5 @@
-se(str):
+#!/usr/bin/python3
+def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             num = 32
